4c18c6056aca9fb742324e22427fc88b00c971c6
mikaeel <mikaeel61@gmail.com>
2017-03-02 16:13:34 -0500

    main file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "This is a test of the git system";
+	return 0;
+}
